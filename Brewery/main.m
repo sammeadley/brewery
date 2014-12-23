031ea48524547449b26ee52c920b0d93bda68c6e
Sam Meadley <sam.meadley@gmail.com>
2014-12-23 06:58:58 -0800

    Initial Commit

---
+++ Brewery/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Brewery
+//
+//  Created by Sam Meadley on 23/12/2014.
+//  Copyright (c) 2014 Sam Meadley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
